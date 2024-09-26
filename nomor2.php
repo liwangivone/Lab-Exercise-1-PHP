36df738fdf6dda23adb96fc35c0f9d4eb4e6f0a6
liwangivone <liwangivone@gmail.com>
2024-09-26 14:04:13 +0800

    update1

--- nomor2.php
+++ nomor2.php
@@ -5,3 +5,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Latihan PHP Nomor 2</title>
 </head>
+<body>
+    <h3>Merging and Sorting Array</h3>
+    <p>Masukkan angka ke dalam array 1:</p>
+    <form method="POST">
+        <input type="number" name="nums1" required>
+        <button type="submit">Tambah</button>
+    </form>
+
+    <p>Masukkan angka ke dalam array 2:</p>
+    <form method="POST">
+        <input type="number" name="nums2" required>
+        <button type="submit">Tambah</button>
+    </form>
+
+    <p>Masukkan nilai m:</p>
+    <form method="POST">
+        <input type="number" name="m" required>
+    </form>
+
+    <p>Masukkan nilai n:</p>
+    <form method="POST">
+        <input type="number" name="n" required>
+    </form>
+
+    <br>
+
+    <button type="submit">Sort & Merge</button>
